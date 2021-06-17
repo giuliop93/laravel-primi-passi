50adef371c825a30d818ec3caf32de10fb5aff41
Giulio_Pa <giuliop93@hotmail.it>
2021-06-17 17:13:59 +0200

    wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -81,7 +81,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello world 
+                    Hello world <br>
+                    {{$nome}}
                 </div>
 
                 <div class="links">
